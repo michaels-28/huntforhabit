fe8e8bf001f82dd032da8f6440c45d2eacb080e2
michaels-28 <michaels_28@live.co.uk>
2022-06-04 18:01:18 +0000

    made app root for home page

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import os
 from flask import(Flask, flash, render_template, redirect, request, session, url_for, Response)
+from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
 
 @app.route("/")
 def test():
@@ -14,6 +20,10 @@ def test():
 @app.route("/test2")
 def test2():
     return render_template("luke.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
