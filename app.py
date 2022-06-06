3824c8d7798b50407e62dac6885acf8d00439d7f
michaels-28 <michaels_28@live.co.uk>
2022-06-06 20:04:28 +0000

    all links work, pages imported

--- app.py
+++ app.py
@@ -25,6 +25,46 @@ def test2():
 def home():
     return render_template("home.html")
 
+@app.route("/online_coaching")
+def online_coaching():
+    return render_template("online_coaching.html")
+
+@app.route("/free_consultation")
+def free_consultation():
+    return render_template("free_consultation.html")
+
+@app.route("/testimonials")
+def testimonials():
+    return render_template("testimonials.html")
+
+@app.route("/calorie_calculator")
+def calorie_calculator():
+    return render_template("calorie_calculator.html")
+
+@app.route("/sign_up")
+def sign_up():
+    return render_template("sign_up.html")
+
+@app.route("/log_in")
+def log_in():
+    return render_template("log_in.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/client_plan")
+def client_plan():
+    return render_template("client_plan.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+@app.route("/faq")
+def faq():
+    return render_template("faq.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
